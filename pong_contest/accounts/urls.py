6cfe49445dad9b1774c53c4a0e188747a0993afa
Martejo <geoffreymartelin@gmail.com>
2024-10-22 16:06:21 +0200

    Adding main and accounts applications

---
+++ pong_contest/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+app_name = 'accounts'  # Nom de l'application pour les noms d'URL
+
+urlpatterns = [
+	path('login/', views.login, name='login'),
+	path('register/', views.register, name='register'),
+	path('loginGuest/', views.logAsguest, name='loginGuest'),
+]
