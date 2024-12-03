0d9d0bdb29e00f462067148a052bcce632ad23b7
Jeffneck <hugot.anglade@gmail.com>
2024-12-03 16:31:27 +0100

    wtf : don t work

--- transcendence/accounts/decorators.py
+++ transcendence/accounts/decorators.py
@@ -1,16 +1,16 @@
 # accounts/decorators.py
 
-#INUTILE le login est verifie avec les JWT 
-# from django.shortcuts import redirect
+from django.shortcuts import redirect
 
-# def login_required(view_func):
-#     def wrapper(request, *args, **kwargs):
-#         # Vérifie si l'utilisateur est connecté
-#         if 'user_id' not in request.session:
-#             return redirect('accounts:login')
-#         # Vérifie si l'authentification 2FA est requise mais non terminée
-#         if request.session.get('auth_partial'):
-#             return redirect('accounts:verify_2fa_login')
-#         return view_func(request, *args, **kwargs)
-#     return wrapper
+# [IMPROVE] ne faudrait il pas recup user ID dans le JWT ?
+def login_required(view_func):
+    def wrapper(request, *args, **kwargs):
+        # Vérifie si l'utilisateur est connecté
+        if 'user_id' not in request.session:
+            return redirect('accounts:login')
+        # Vérifie si l'authentification 2FA est requise mais non terminée
+        if request.session.get('auth_partial'):
+            return redirect('accounts:verify_2fa_login')
+        return view_func(request, *args, **kwargs)
+    return wrapper
 
