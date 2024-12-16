d54b55ee5d8d6b4df973ee5db0c788115ed21114
Hugo <hugot.anglade@gmail.com>
2024-12-16 08:06:25 -0500

    some changes in js

--- transcendence/static/js/tools/domHandler.js
+++ transcendence/static/js/tools/domHandler.js
@@ -77,6 +77,7 @@ export function updateHtmlContent(selector, html) {
         // Mise à jour du contenu à partir des données reçues
         document.querySelector(selector).innerHTML = html;
     } catch (error) {
+        //[IMPROVE] faire un autre throw ici pour gerer l' erreur dans les fonctions specialisees
         document.querySelector(selector).innerHTML = '<p>Une erreur est survenue lors du chargement du contenu.</p>';
     }
 }
