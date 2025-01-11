b4aa484c2c6db150e2eb7e02b45577255050eb57
Hamza EL OUARDI <hamzaelouardi@mac.home>
2025-01-11 23:22:59 +0100

    language ok, juste a voir ou mettre les bouton

--- transcendence/transcendence/settings.py
+++ transcendence/transcendence/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'accounts.middleware.OnlineStatusMiddleware', #Notre middleware
 	'django.middleware.locale.LocaleMiddleware',
+	'core.middleware.SetLanguageRedirectMiddleware',
 	# 'django.template.context_processors.csrf',  # Assurez-vous que celui-ci est présent
 
 ]
